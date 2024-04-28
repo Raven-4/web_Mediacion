5ad7ae9ef1a58264e1eb509e6cde9777d51e712c
Raven-4 <121634522+Raven-4@users.noreply.github.com>
2024-04-28 12:12:27 +0200

    backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,27 +1,31 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-// Conexión a la base de datos SQLite
-const dbPath = path.join(__dirname, '../DB/mediacion.db');
-const db = new sqlite3.Database(dbPath);
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
 
-// Ruta para obtener todos los datos de una tabla específica
-app.get('/datos/:tabla', (req, res) => {
-    const tabla = req.params.tabla;
-    db.all(`SELECT * FROM ${tabla}`, (err, rows) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.send(rows);
-        }
-    });
-});
+app.use(express.json({
+    type: "*/*"
+}))
 
-// Inicia el servidor
+app.use(cors());
+
+// GET obtiene todas las transacciones
+app.get('/transactions', (req, res) => {
+    res.send(`me hicieron un get`);
+})
+
+// POST guarda los datos de una transacción
+app.post('/transactions', (req, res) => {
+    res.send(`me hicieron un post`);
+})
+
 app.listen(port, () => {
     console.log(`Servidor en ejecución en http://localhost:${port}`);
-});
+})
